0c6b4af8b0f734b75e5bfa2de1c862af08a0b360
dinhtri206 <tridead1997@gmail.com>
2025-09-24 22:59:32 +0700

    Add files via upload

---
+++ Docso.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Docso {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Nhập N (0-9): ");
+        int n = sc.nextInt();
+
+        switch (n) {
+            case 0: System.out.println("Không"); break;
+            case 1: System.out.println("Một"); break;
+            case 2: System.out.println("Hai"); break;
+            case 3: System.out.println("Ba"); break;
+            case 4: System.out.println("Bốn"); break;
+            case 5: System.out.println("Năm"); break;
+            case 6: System.out.println("Sáu"); break;
+            case 7: System.out.println("Bảy"); break;
+            case 8: System.out.println("Tám"); break;
+            case 9: System.out.println("Chín"); break;
+            default:
+                System.out.println("Lỗi: vui lòng nhập số tự nhiên có một chữ số (0-9).");
+        }
+
+        sc.close();
+    }
+}
